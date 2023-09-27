29eff3181cd894aad11835659b630e67417cd148
Dmitry <dimactik222@gmail.com>
2023-09-27 00:06:48 -0400

    PizzaBlock, Header, Categories created

---
+++ src/components/Categories.tsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+function Categories() {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const categories = ["Всё", "Мясные", "Вегетарианская", "Гриль", "Острые", "Закрытые"];
+
+  const onClickCategory = (index) => {
+    setActiveIndex(index);
+  };
+
+  return (
+    <div className="categories">
+      <ul>
+        {categories.map((value, index) => (
+          <li onClick={() => onClickCategory(index)} className={activeIndex === index ? "active" : ""}>
+            {value}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Categories;
