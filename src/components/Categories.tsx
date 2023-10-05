80f79a84d08ecfc1dd124dafd82a2d01c24b6ae7
Dmitry <dimactik222@gmail.com>
2023-10-05 15:10:58 -0400

    Sort and Category developed

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -1,20 +1,14 @@
 import { useState } from "react";
 
-function Categories() {
-  const [activeIndex, setActiveIndex] = useState(0);
-
+function Categories({ value, onChangeCategory }) {
   const categories = ["Всё", "Мясные", "Вегетарианская", "Гриль", "Острые", "Закрытые"];
-
-  const onClickCategory = (index) => {
-    setActiveIndex(index);
-  };
 
   return (
     <div className="categories">
       <ul>
-        {categories.map((value, index) => (
-          <li key={index} onClick={() => onClickCategory(index)} className={activeIndex === index ? "active" : ""}>
-            {value}
+        {categories.map((categoryName, index) => (
+          <li key={index} onClick={() => onChangeCategory(index)} className={value === index ? "active" : ""}>
+            {categoryName}
           </li>
         ))}
       </ul>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,9 +7,17 @@ import Sort from "../components/Sort";
 const Home = () => {
   const [items, setItems] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [categoryId, setCategoryId] = useState(0);
+  const [sortType, setSortType] = useState({ name: "популярности", sortProperty: "rating" });
 
   useEffect(() => {
-    fetch("https://63c610b5e1292e5bea2f5ad2.mockapi.io/pizza")
+    setIsLoading(true);
+
+    const category = categoryId > 0 ? `category=${categoryId}` : "";
+    const sortBy = sortType.sortProperty.replace("-", "");
+    const order = sortType.sortProperty.includes("-") ? "asc" : "desc";
+
+    fetch(`https://63c610b5e1292e5bea2f5ad2.mockapi.io/pizza?${category}&sortBy=${sortBy}&order=${order}`)
       .then((res) => {
         return res.json();
       })
@@ -18,12 +26,12 @@ const Home = () => {
         setIsLoading(false);
       });
     window.scrollTo(0, 0);
-  }, []);
+  }, [categoryId, sortType]);
   return (
     <>
       <div className="content__top">
-        <Categories />
-        <Sort />
+        <Categories value={categoryId} onChangeCategory={(i) => setCategoryId(i)} />
+        <Sort value={sortType} onChangeSort={(i) => setSortType(i)} />
       </div>
       <h2 className="content__title">Все пиццы</h2>
       <div className="content__items">
